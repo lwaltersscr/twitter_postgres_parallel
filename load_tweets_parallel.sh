5daccaade4bd283e2a77cce0f8feb428a60ec904
Lindsey <lindseywltrs@gmail.com>
2025-04-25 21:56:47 -0700

    fixed parralel

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,15 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-echo "$files" | time parallel ./load_denormalized.sh
+echo "$files" | time parallel ./load_denormalized.sh {}
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 ./load_tweets.py --db postgresql://postgres:pass@localhost:4721 --inputs $file
+echo "$files" | time parallel python3 ./load_tweets.py --db postgresql://postgres:pass@localhost:6977 --inputs {}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 ./load_tweets_batch.py --db postgresql://postgres:pass@localhost:4722 --inputs $file
+echo "$files" | time parallel python3 ./load_tweets_batch.py --db postgresql://postgres:pass@localhost:6978 --inputs {}
+
